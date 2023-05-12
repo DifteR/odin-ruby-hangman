63bf0ce5f95f7028c396e6fdf82474a08e75891e
Tilen Marš <its0black0magic@gmail.com>
2023-05-12 13:58:29 +0000

    Added method to draw hangman

--- main.rb
+++ main.rb
@@ -1,20 +1,82 @@
 class Hangman
-    attr_reader :correctWord
-    attr_accessor :guess
-    def initialize
-        @correctWord = File.readlines("lib/google-10000-english-no-swears.txt").sample
-        @guess = Array.new(correctWord.length)
-    end
+  attr_reader :correctWord
+  attr_accessor :guess
+  
+  def initialize
+    @correctWord = File.readlines("lib/google-10000-english-no-swears.txt").sample
+    @guess = Array.new(correctWord.length)
+  end
 
-    def checkMatchingLetters(a)
-        i = 0
-        while i < correctWord.length
-            if correctWord[i] == a
-                guess[i] = a
-            end
-            i += 1
-        end
+  def checkMatchingLetters(a) #could rewrite using enumrables
+    i = 0
+    while i < correctWord.length
+      if correctWord[i] == a
+        guess[i] = a
+      end
+      i += 1
     end
+  end
+
+  def draw_hangman(guesses)
+    case guesses
+    when 0
+      puts " _________"
+      puts "|         |"
+      puts "|         "
+      puts "|          "
+      puts "|           "
+      puts "|          "
+      puts "|"
+    when 1
+      puts " _________"
+      puts "|         |"
+      puts "|         O"
+      puts "|          "
+      puts "|           "
+      puts "|          "
+      puts "|"
+    when 2
+      puts " _________"
+      puts "|         |"
+      puts "|         O"
+      puts "|         |"
+      puts "|           "
+      puts "|          "
+      puts "|"
+    when 3
+      puts " _________"
+      puts "|         |"
+      puts "|         O"
+      puts "|        /|"
+      puts "|           "
+      puts "|          "
+      puts "|"
+    when 4
+      puts " _________"
+      puts "|         |"
+      puts "|         O"
+      puts "|        /|\\"
+      puts "|           "
+      puts "|          "
+      puts "|"
+    when 5
+      puts " _________"
+      puts "|         |"
+      puts "|         O"
+      puts "|        /|\\"
+      puts "|        /  "
+      puts "|          "
+      puts "|"
+    else
+      puts " _________"
+      puts "|         |"
+      puts "|         O"
+      puts "|        /|\\"
+      puts "|        / \\"
+      puts "|          "
+      puts "|"
+    end
+  end
 end
 
 game = Hangman.new()
