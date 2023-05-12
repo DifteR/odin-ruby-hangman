d30e747d9c77ac02bf78f0b28f76f725cbdd1694
Tilen Marš <its0black0magic@gmail.com>
2023-05-12 12:00:06 +0000

    initialize project

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
+class Hangman
+    def initialize
+        attr_reader :correctWord
+        correctWord = File.readlines('lib/google-10000-english-no-swears.txt').sample
+    end
+end
 
+game = Hangman.new()
+puts game.correctWord
