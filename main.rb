dfe776e2afe200d4b002c2e2859b19173a22b1b6
Tilen Marš <its0black0magic@gmail.com>
2023-05-12 13:02:35 +0000

    add guess variable, length of random word

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 class Hangman
+    attr_reader :correctWord
+    attr_accessor :guess
     def initialize
-        attr_reader :correctWord
-        correctWord = File.readlines('lib/google-10000-english-no-swears.txt').sample
+        @correctWord = File.readlines("lib/google-10000-english-no-swears.txt").sample
+        @guess = Array.new(correctWord.length)
     end
 end
 
 game = Hangman.new()
-puts game.correctWord
